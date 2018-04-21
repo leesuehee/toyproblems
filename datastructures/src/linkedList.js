b628ca021ba04c9be3c57776abadb99f5f639f74
leesuehee <sueheelee@outlook.com>
2018-04-21 14:19:07 -0700

    adds to tail removes head

--- datastructures/src/linkedList.js
+++ datastructures/src/linkedList.js
@@ -1,36 +1,57 @@
 
 let Node = function(value) {
-  var node = {};
+  let content = {};
 
-  node.value = value;
-  node.next = null;
+  content.value = value;
+  content.next = null;
 
-  return node;
+  return content;
 };
 
 let LinkedList = function() {
   let list = {};
+
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
-
-  }
-  list.removeHead = function() {
-    let hold = this.head.value;
-    this.head = this.head.next;
-    console.log(this.head)
-    return hold
+    let node = Node(value);
+    
+    if (!this.head && !this.tail) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = this.tail.next;
+    };
   };
 
-  list.contains = function(target) {
-    
+  list.removeHead = function(value) {
+    let holdHead = this.head.value;
+
+    this.head = this.head.next;
+    return holdHead;
+  };
+
+  list.contains = function(target) {  
+  let temp = list;
+
+  let checkNext = function(temp) {
+    if (temp.head.value === target) {
+      return true; 
+    } else {
+      if (temp.head.next) {
+        checkNext(temp.head.next);
+      } else {
+        return false;
+      };
+    };
+  };
+
+  return checkNext(this);
   };
 
   return list;
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
 
