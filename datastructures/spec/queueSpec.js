dd7a259f27b839614dd733baaa810428c230d955
leesuehee <sueheelee@outlook.com>
2018-04-26 14:14:55 -0700

    Adds tests for queue

---
+++ datastructures/spec/queueSpec.js
@@ -0,0 +1,43 @@
+describe('queue', function() {
+  var queue;
+
+  beforeEach(function() {
+    queue = new Queue();
+  });
+
+  it('should the methods "enqueue", "dequeue", "size", and "contains"', function() {
+    expect(queue.enqueue).to.be.a('function');
+    expect(queue.dequeue).to.be.a('function');
+    expect(queue.contains).to.be.a('function');
+    expect(queue.size).to.be.a('function');
+  });
+
+  it('should add values to the end of the queue', function(){
+    queue.enqueue(5);
+    expect(queue.size).to.equal(1);
+    queue.enqueue(2);
+    expect(queue.size).to.equal(2);
+  });
+
+  
+  it('should remove values from the front queue', function(){
+    queue.enqueue(5);
+    queue.enqueue(2);
+    expect(queue.size).to.equal(2);
+    expect(queue.dequeue()).to.equal(5);
+  });
+
+  it('should return a boolean whether input value is in the queue', function(){
+    queue.enqueue(7);
+    queue.enqueue(2);
+    expect(queue.contains(7)).to.equal(true);
+    expect(queue.contains(1)).to.equal(false);
+  });
+
+  it('should determine the size of a queue', function(){
+    queue.enqueue(5);
+    queue.enqueue(2);
+    expect(queue.size).to.equal(2);
+  });
+ 
+});
