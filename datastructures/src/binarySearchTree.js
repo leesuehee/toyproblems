e02a662c40700dc112d494c2df9bf484550e5fce
leesuehee <sueheelee@outlook.com>
2018-05-04 14:58:00 -0700

    adds insert method to binary tree

--- datastructures/src/binarySearchTree.js
+++ datastructures/src/binarySearchTree.js
@@ -1,7 +1,49 @@
 var BinarySearchTree = function(value) {
+  var binaryTree = {};
+  
+  binaryTree.value = value;
+  binaryTree.right = null;
+  binaryTree.left  = null;
+  
+  binaryTree.insert = function(value) {
+    let node = BinarySearchTree(value);  
+
+    let recurse = function (tree) {
+      if (value > tree.value) {
+        if (tree.right === null) {
+          tree.right = node;
+          return tree;
+        } else { 
+          recurse(tree.right) 
+        };   
+      } else {
+        if (tree.left === null) {
+          tree.left = node;
+          return tree;  
+        } else { 
+          recurse(tree.left) 
+        }
+      };
+    };  
+
+    recurse(this);
+  }; 
+  
+  binaryTree.contains = function(value) {
+  // traverse entire tree the moment the value is found return a boolean
+  // basecase if when all the roots are reached or a value is met
+  
+
+  };
+  
+  binaryTree.depthFirstLog = function() {
+    
+  };
+      
+  return binaryTree;
 };
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: 
  */
--- datastructures/src/tree.js
+++ datastructures/src/tree.js
@@ -29,6 +29,3 @@ var Tree = function(value) {
 
 
 
-/*
- * N 
- */
